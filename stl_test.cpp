c7595a34090ac6c5932754b07f467930d3bc59de
owen <cactus5057@gmail.com>
2023-01-31 10:28:00 +0900

    v1.31.3

--- stl_test.cpp
+++ stl_test.cpp
@@ -3,15 +3,25 @@
 #include <unordered_map>
 #include <string>
 
+void test_unoredered_map();
+void test_map();
+
 int main() {
+    test_unoredered_map();
+    test_map();
+}
+
+void test_unoredered_map(){
+    std::cout << "\n**Unoredered Map Test Start------------------------";
+
     std::unordered_map<std::string, int> um;
 
     um.insert(std::pair<std::string, int>("one", 10));
     um.insert(std::make_pair("two", 22));
-    um["third"] = 34;
-    um.insert(std::make_pair("forth", 42));
+    um["three"] = 34;
+    um.insert(std::make_pair("four", 42));
 
-    std::cout << "\n------------------------\n";
+    std::cout << "\n*search v1------------------------\n";
 
     std::unordered_map<std::string, int>::iterator it;
 
@@ -19,11 +29,102 @@ int main() {
         std::cout << it->first << "/" << it->second << std::endl;
     }
 
-    std::cout << "\n------------------------\n";
+    std::cout << "\n*search v2------------------------\n";
 
     for(auto pair: um){
         std::cout << pair.first << "/" << pair.second << std::endl;
     } //why auto pair can't use '->' ?
 
-    
+    std::cout << "\n*Find------------------------\n";
+
+    std::cout << um.find("two")->first << "'s value : " << um.find("two")->second << std::endl;
+    std::cout << um.find("four")->first << "'s value : " << um.find("four")->second << std::endl;
+
+    std::cout << "\n*Size------------------------\n";
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << "\n*Erase------------------------\n";
+
+    um.erase("two"); std::cout << "erase two" << std::endl;
+    um.erase("three"); std::cout << "erase three" << std::endl;
+    // there are no other way to erase object of unoredered_map
+
+    for(auto pair: um){
+        std::cout << pair.first << "/" << pair.second << std::endl;
+    }
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << "\n*Empty------------------------\n";
+
+    std::cout << (um.empty() ? "Empty!" : "Not Empty!") << std::endl;
+
+    um.erase("one"); std::cout << "erase one" << std::endl;
+    um.erase("four"); std::cout << "erase four" << std::endl;
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << (um.empty() ? "Empty!" : "Not Empty!") << std::endl;
+
+    std::cout << "\n**Unoredered Map Test End------------------------\n\n";
+}
+
+void test_map(){
+    std::cout << "\n**Map Test Start------------------------";
+
+    std::map<std::string, int> um;
+
+    um.insert(std::pair<std::string, int>("one", 10));
+    um.insert(std::make_pair("two", 22));
+    um["three"] = 34;
+    um.insert(std::make_pair("four", 42));
+
+    std::cout << "\n*search v1------------------------\n";
+
+    std::map<std::string, int>::iterator it;
+
+    for(it = um.begin(); it != um.end(); it++){
+        std::cout << it->first << "/" << it->second << std::endl;
+    }
+
+    std::cout << "\n*search v2------------------------\n";
+
+    for(auto pair: um){
+        std::cout << pair.first << "/" << pair.second << std::endl;
+    } //why auto pair can't use '->' ?
+
+    std::cout << "\n*Find------------------------\n";
+
+    std::cout << um.find("two")->first << "'s value : " << um.find("two")->second << std::endl;
+    std::cout << um.find("four")->first << "'s value : " << um.find("four")->second << std::endl;
+
+    std::cout << "\n*Size------------------------\n";
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << "\n*Erase------------------------\n";
+
+    um.erase("two"); std::cout << "erase two" << std::endl;
+    um.erase("three"); std::cout << "erase three" << std::endl;
+    // there are no other way to erase object of unoredered_map
+
+    for(auto pair: um){
+        std::cout << pair.first << "/" << pair.second << std::endl;
+    }
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << "\n*Empty------------------------\n";
+
+    std::cout << (um.empty() ? "Empty!" : "Not Empty!") << std::endl;
+
+    um.erase("one"); std::cout << "erase one" << std::endl;
+    um.erase("four"); std::cout << "erase four" << std::endl;
+
+    std::cout << "Now size: " << um.size() << std::endl;
+
+    std::cout << (um.empty() ? "Empty!" : "Not Empty!") << std::endl;
+
+    std::cout << "\n**Map Test End------------------------\n\n";
 }
